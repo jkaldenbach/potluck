dc66279f31e2414ca55e1bd6b81a422d7f06f5c7
John Saady <john.saady@gooddonegreat.com>
2016-04-23 00:45:17 -0400

    Models

--- pots/models.py
+++ pots/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pot(models.Model):
+    color = models.CharField(max_length=6)
+    size = models.CharField()
+
+class Location(models.Model):
+    pot = models.ForeignKey('Pot')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    timestamp = models.DateTimeField()
