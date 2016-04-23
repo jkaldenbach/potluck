61d42eade015d498e806688c9af4fca5d515e5dc
John Saady <john.saady@gooddonegreat.com>
2016-04-23 00:21:43 -0400

    Added new app and static dir

--- potluck/settings.py
+++ potluck/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pots',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -98,5 +99,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATIC_URL = '/static/'
