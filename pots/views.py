59b1e7773c1c59ad2c2e62e78786f2e8b341eda8
John Saady <john.saady@gooddonegreat.com>
2016-04-24 12:01:27 -0400

    CSRF

--- pots/views.py
+++ pots/views.py
@@ -9,14 +9,20 @@ from .models import Pot, Location, Deployment, Fisher
 from .serializers import PotSerializer, LocationSerializer, DeploymentSerializer, FisherSerializer
 import services
 
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
 class PotViewSet(viewsets.ModelViewSet):
     queryset = Pot.objects.all()
     serializer_class = PotSerializer
 
+@csrf_exempt
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all().order_by('-timestamp')
     serializer_class = LocationSerializer
 
+@csrf_exempt
 class DeploymentViewSet(viewsets.ModelViewSet):
     queryset = Deployment.objects.all()
     serializer_class = DeploymentSerializer
@@ -33,10 +39,12 @@ class DeploymentViewSet(viewsets.ModelViewSet):
         serializer = DeploymentSerializer(lost_pots, many=True)
         return Response(serializer.data)
 
+@csrf_exempt
 class FisherViewSet(viewsets.ModelViewSet):
     queryset = Fisher.objects.all()
     serializer_class = FisherSerializer
 
+@csrf_exempt
 def getStormData(request):
         return HttpResponse(
             json.dumps(services.get_storm_data(request.GET['start'], request.GET['end']))
